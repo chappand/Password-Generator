773a48a60aff70fada4f8d05c92e4d69a50d6c19
Andrew Chappell <chappand@gmail.com>
2020-12-11 16:40:13 -0500

    Added arrays for each character. Added variable to store password length question. Started the process for confirming lowercase question.

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercase = ["a", "b", "c"];
+var uppercase = ["A", "B", "C"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
+
+function generatePassword() {
+//Array to store random characters//
+  var possibleRandomChar = [];
+//Prompt for password length//
+  var passwordLength = prompt("How long do you want your password?");
+  console.log(passwordLength);
+
+  if(passwordLength < 8){
+    alert("Your password must be more than 8 characters");
+    return;
+  }
+
+  if(passwordLength > 128){
+    alert("Your password must be less than 128 characters");
+    return;
+  }
+//Confirm for lowercase characters
+  var containLower = confirm("Would you like to include lowercase characters?");
+  console.log(containLower);
+
+  if(containLower){
+    possibleRandomChar = possibleRandomChar.concat(lowercase);
+    //take the array and pass it through a random geneator function and then push value to array
+    console.log(possibleRandomChar);
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
